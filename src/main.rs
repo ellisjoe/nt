d7b44e58d94756cda456ea36fe608dceb6ddd4aa
Joe Ellis <ellis125@gmail.com>
2026-07-15 22:54:33 -0600

    Get basic tcp/udp traffic working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,134 @@
+use std::io;
+use std::io::{stdin, Read, Write};
+use clap::{ArgGroup, Parser};
+use std::net::{TcpListener, TcpStream, UdpSocket};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+#[command(allow_missing_positional = true)]
+#[command(group(
+    ArgGroup::new("protocol")
+        .args(["tcp", "udp"])
+        .multiple(false)
+))]
+struct Args {
+    #[arg(short, long)]
+    listen: bool,
+
+    #[arg(short, long)]
+    tcp: bool,
+
+    #[arg(short, long)]
+    udp: bool,
+
+    #[arg(required_unless_present = "listen")]
+    host: Option<String>,
+
+    port: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let mode = get_mode(&args);
+
+    let host = args.host.unwrap_or("0.0.0.0".to_string());
+    let port = args.port;
+    let address = format!("{}:{}", host, port);
+
+    if args.listen {
+        let mut receiver = mode.get_receiver(address);
+
+        let mut buf = [0; 1024];
+        loop {
+            let num = receiver.read(&mut buf).unwrap();
+            let result = str::from_utf8(&buf[..num]).unwrap();
+            print!("{}", result);
+        }
+    } else {
+        let mut sender = mode.get_sender(address);
+
+        loop {
+            let mut input = String::new();
+            stdin().read_line(&mut input).unwrap();
+            let bytes = input.as_bytes();
+
+            let mut sent = 0;
+            while sent < bytes.len() {
+                sent = sender.send(&bytes[sent..]).unwrap();
+            }
+        }
+    }
+}
+
+fn get_mode(args: &Args) -> Protocol {
+    if args.udp {
+        Protocol::Udp
+    } else {
+        Protocol::Tcp
+    }
+}
+
+enum Protocol {
+    Udp,
+    Tcp,
+}
+
+impl Protocol {
+    fn get_sender(&self, address: String) -> Box<dyn Sender> {
+        match self {
+            Protocol::Udp => {
+                let udp = Box::new(UdpSocket::bind("0.0.0.0:0").unwrap());
+                udp.connect(address).unwrap();
+                udp
+            }
+            Protocol::Tcp => {
+                let tcp = TcpStream::connect(address);
+                Box::new(tcp.unwrap())
+            }
+        }
+    }
+
+    fn get_receiver(&self, address: String) -> Box<dyn Receiver> {
+        match self {
+            Protocol::Udp => {
+                Box::new(UdpSocket::bind(address).unwrap())
+            }
+            Protocol::Tcp => {
+                let listener = TcpListener::bind(address).unwrap();
+                Box::new(listener.accept().unwrap().0)
+            }
+        }
+    }
+}
+
+trait Sender: Receiver {
+    fn send(&mut self, buf: &[u8]) -> io::Result<usize>;
+}
+
+trait Receiver {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize>;
+}
+
+impl Sender for UdpSocket {
+    fn send(&mut self, buf: &[u8]) -> io::Result<usize> {
+        UdpSocket::send(self, buf)
+    }
+}
+
+impl Sender for TcpStream {
+    fn send(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.write(buf)
+    }
+}
+
+impl Receiver for UdpSocket {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.recv_from(buf).map(|(n, _)| n)
+    }
+}
+
+impl Receiver for TcpStream {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        Read::read(self, buf)
+    }
 }

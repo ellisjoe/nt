f2861155319bbe2be219ad7f1aefa9125708c3fe
Joe Ellis <ellis125@gmail.com>
2026-07-16 14:21:48 -0600

    Add Mutlicast support

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use crate::UdpMode::{Broadcast, Multicast, Unicast};
 use clap::{ArgGroup, Parser};
 use std::io;
-use std::io::{Read, Write, stdin};
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, TcpListener, TcpStream, ToSocketAddrs, UdpSocket};
+use std::io::{stdin, Read, Write};
+use std::net::{
+    Ipv4Addr, TcpListener, TcpStream, UdpSocket,
+};
 
 const ALL_INTERFACES: &str = "0.0.0.0";
 
@@ -35,10 +38,9 @@ fn main() -> io::Result<()> {
 
     let host = args.host.unwrap_or(ALL_INTERFACES.to_string());
     let port = args.port;
-    let address = format!("{}:{}", host, port);
 
     if args.listen {
-        let receiver = mode.get_receiver(address)?;
+        let receiver = mode.get_receiver(host, port)?;
 
         let mut buf = [0; 1024];
         loop {
@@ -47,7 +49,7 @@ fn main() -> io::Result<()> {
             println!("{}", result.trim());
         }
     } else {
-        let sender = mode.get_sender(address)?;
+        let sender = mode.get_sender(host, port)?;
 
         loop {
             let mut input = String::new();
@@ -56,7 +58,7 @@ fn main() -> io::Result<()> {
 
             let mut sent = 0;
             while sent < bytes.len() {
-                sent = sender.send(&bytes[sent..])?;
+                sent = sender.send(&bytes[sent..]).unwrap();
             }
         }
     }
@@ -75,27 +77,58 @@ enum Protocol {
     Tcp,
 }
 
+enum UdpMode {
+    Unicast,
+    Multicast(Ipv4Addr),
+    Broadcast,
+}
+
+impl From<&str> for UdpMode {
+    fn from(host: &str) -> Self {
+        if let Ok(ip) = host.parse::<Ipv4Addr>() {
+            if ip.is_multicast() {
+                return Multicast(ip);
+            } else if ip.is_broadcast() {
+                return Broadcast;
+            }
+        }
+        Unicast
+    }
+}
+
 impl Protocol {
-    fn get_sender(&self, address: String) -> io::Result<Box<dyn Sender>> {
+    fn get_sender(&self, host: String, port: u16) -> io::Result<Box<dyn Sender>> {
         match self {
             Protocol::Udp => {
                 let udp = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0))?;
-                udp.connect(address)?;
 
-                if let IpAddr::V4(v4) = udp.peer_addr()?.ip() && v4.is_broadcast() {
-                    udp.set_broadcast(true)?;
+                match host.as_str().into() {
+                    Unicast => {},
+                    Multicast(ip) => udp.join_multicast_v4(&ip, &Ipv4Addr::UNSPECIFIED)?,
+                    Broadcast => udp.set_broadcast(true)?,
                 }
 
+                udp.connect((host, port))?;
                 Ok(Box::new(udp))
             }
-            Protocol::Tcp => Ok(Box::new(TcpStream::connect(address)?)),
+            Protocol::Tcp => Ok(Box::new(TcpStream::connect((host, port))?)),
         }
     }
 
-    fn get_receiver(&self, address: String) -> io::Result<Box<dyn Receiver>> {
+    fn get_receiver(&self, host: String, port: u16) -> io::Result<Box<dyn Receiver>> {
         match self {
-            Protocol::Udp => Ok(Box::new(UdpSocket::bind(address)?)),
-            Protocol::Tcp => Ok(Box::new(TcpListener::bind(address)?.accept()?.0)),
+            Protocol::Udp => {
+                let mode: UdpMode = host.as_str().into();
+
+                if let Multicast(ip) = mode {
+                    let udp = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, port))?;
+                    udp.join_multicast_v4(&ip, &Ipv4Addr::UNSPECIFIED)?;
+                    return Ok(Box::new(udp));
+                }
+
+                Ok(Box::new(UdpSocket::bind((host, port))?))
+            },
+            Protocol::Tcp => Ok(Box::new(TcpListener::bind((host, port))?.accept()?.0)),
         }
     }
 }

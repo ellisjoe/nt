e99a51ea045b474112444e3ccd987e86b43acd1e
Joe Ellis <ellis125@gmail.com>
2026-08-02 08:36:33 -0600

    Change port to one not used on GH action runner

--- src/main.rs
+++ src/main.rs
@@ -123,7 +123,7 @@ mod tests {
     static LOCALHOST: &str = "127.0.0.1";
     static MULTICAST: &str = "224.0.0.251";
     static BROADCAST: &str = "255.255.255.255";
-    static PORT: u16 = 1111;
+    static PORT: u16 = 8888;
 
     #[test]
     fn test_udp_localhost() -> Result<()> {
